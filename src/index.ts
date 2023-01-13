6f2bfb9e486ad6b26fecb75240116281635e0f3d
Pavel Gorbach <polgorbach@gmail.com>
2023-01-13 15:17:35 +0300

    feat: implement server; add GET users endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
-import { v4 as uuidv4 } from 'uuid'
+import http from 'node:http'
+import dotenv from 'dotenv'
 
-console.log('Hello CRUD!', uuidv4())
+import { getUsers } from './users'
+
+dotenv.config()
+
+const host = 'localhost'
+const port = Number(process.env.PORT) || 8000
+
+const server = http.createServer()
+
+server.on('request', (req, resp) => {
+  const { method, url } = req
+
+  resp.writeHead(200, { 'Content-Type': 'application/json' })
+
+  if (method === 'GET') {
+    switch (url) {
+      case '/api/users': {
+        const users = getUsers()
+
+        resp.end(
+          JSON.stringify({
+            data: users
+          })
+        )
+        break
+      }
+      default:
+        resp.writeHead(404)
+        resp.end(
+          JSON.stringify({
+            data: 'Not Found'
+          })
+        )
+    }
+  }
+})
+
+server.listen(port, host, () => {
+  console.log(`listening on http://${host}:${port}`)
+})
