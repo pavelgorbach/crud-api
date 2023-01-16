31cb5f3b776142425d4d946086b30699d3e54fd1
Pavel Gorbach <polgorbach@gmail.com>
2023-01-16 21:54:56 +0300

    feat: different ports for each worker

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import http from 'node:http'
 import dotenv from 'dotenv'
 import process from 'node:process'
-import cluster from 'node:cluster'
+import cluster, { Worker } from 'node:cluster'
 import { cpus } from 'node:os'
 
 import { handleRoute } from './routes'
 
 dotenv.config()
-
-const port = Number(process.env.PORT)
 
 export function createApp() {
   const server = http.createServer()
@@ -18,26 +16,34 @@ export function createApp() {
 
 if (process.env.NODE_ENV === 'production') {
   const app = createApp()
-  app.listen(port)
+  app.listen(Number(process.env.PORT))
 }
 
 if (process.env.NODE_ENV === 'multi') {
   const numCPUs = cpus().length
 
   if (cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running on port: ${port}`)
+    const pidToPort: { [id: number]: number } = {}
+    let worker: Worker, port
 
     for (let i = 0; i < numCPUs; i++) {
-      cluster.fork()
+      port = 4000 + i
+      worker = cluster.fork({ port: port })
+
+      if (worker.process.pid) {
+        pidToPort[worker.process.pid] = port
+      }
     }
 
-    cluster.on('exit', (worker) => {
-      console.log(`worker ${worker.process.pid} died`)
+    console.log(pidToPort)
+
+    cluster.on('exit', function (worker) {
+      console.log('worker ' + worker.process.pid + ' died')
     })
   } else {
     const app = createApp()
-    app.listen(port)
+    app.listen(process.env.port)
 
-    console.log(`Worker ${process.pid} started on port: ${port}`)
+    console.log(`Worker ${process.pid} started on port: ${process.env.port}`)
   }
 }
